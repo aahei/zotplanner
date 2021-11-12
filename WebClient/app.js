2ba1ec16b273ef79a6f089819a26bd53e5adbb9a
ZhiYuan Wang <zhiyuanwang1003@gmail.com>
2021-11-12 03:57:47 -0800

    Update webclient to react environment

--- WebClient/app.js
+++
@@ -1,4 +0,0 @@
-const title = React.createElement('h1', null, "ZotPlanner Coming Soom" )
-const center = React.createElement('center', null, title )
-ReactDOM.render(center, document.getElementById('root'))
-//hello
